c65a9d3fc3a0add74de383956ceca353afcb5491
taeng42 <76938720+taeng42@users.noreply.github.com>
2023-09-11 23:46:35 +0900

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,47 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
+  {
+    "ellisonleao/glow.nvim",
+    event = "VimEnter",
+    opts = {
+      -- glow_path = "", -- will be filled automatically with your glow bin in $PATH, if any
+      -- install_path = "/home/hyulim/.local/bin", -- default path for installing glow binary
+      border = "shadow", -- floating window border config
+      style = "dark", -- filled automatically with your current editor background, you can override using glow json style
+      pager = false,
+      width = 80,
+      height = 100,
+      width_ratio = 0.7, -- maximum width of the Glow window compared to the nvim window size (overrides `width`)
+      height_ratio = 0.7,
+    },
+  },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  {
+    "folke/todo-comments.nvim",
+    -- sudo apt install ripgrep
+    -- brew install ripgrep
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  { -- This plugin
+    "Zeioth/compiler.nvim",
+    cmd = {"CompilerOpen", "CompilerToggleResults", "CompilerRedo"},
+    dependencies = { "stevearc/overseer.nvim" },
+    opts = {},
+  },
+  { -- The task runner we use
+    "stevearc/overseer.nvim",
+    commit = "19aac0426710c8fc0510e54b7a6466a03a1a7377",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    opts = {
+      task_list = {
+        direction = "bottom",
+        min_height = 25,
+        max_height = 25,
+        default_detail = 1,
+        bindings = { ["q"] = function() vim.cmd("OverseerClose") end },
+      },
+    },
+  },
 }
