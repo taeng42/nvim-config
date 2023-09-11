2e6309418c4d3f8d970924d1baf1f7d29a226ebe
taeng42 <76938720+taeng42@users.noreply.github.com>
2023-09-11 20:26:51 +0900

    Update options.lua
    for NOW!! INDENT is 4!! 4!! NOT 2!!!!

--- options.lua
+++ options.lua
@@ -1,1 +1,8 @@
 vim.o.shell = "bash"
+return {
+  opt = {
+    shiftwidth = 4, -- number of space inserted for indentation
+    showtabline = 4, -- always display tabline
+    tabstop = 4, -- number of space in a tab
+    },
+}
